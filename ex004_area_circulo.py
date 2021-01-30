6f9569c993a926e99e8448a383d8d27699190531
Daniel Tisoy <58006592+luis-dt@users.noreply.github.com>
2021-01-30 12:04:01 -0500

    ejercicio 4: calcular area de un circulo

--- ex004_area_circulo.py
+++ ex004_area_circulo.py
@@ -1,11 +1,11 @@
 # Ejercicio 4: Solicitar el valor del radio de un circulo para calcular su área.
-# 1
+# solucion 1
 from math import pi
 radio = float(input('radio del circulo: '))
 area = pi * (radio**2)
 print(f'El area del circulo con radio {radio} es: {area}')
 
-# 2
+# solucion 2
 pi1 = 3.1416
 area = pi1 * (radio**2)
 print(f'El area del cirulo con radio {radio} es: {area}')
