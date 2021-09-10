7905fb01af1e973c5b1ad3e1c6403ac3cec35ad4
ltisoy <danieltisoy2001@gmail.com>
2021-09-10 08:40:20 -0500

    Logo de los juegos olimpicos

---
+++ olympic_logo.py
@@ -0,0 +1,18 @@
+import turtle as p
+
+def draw_circle(x,y,c='red'):
+    p.pu()
+    p.goto(x,y)
+    p.pd()
+    p.color(c)
+    p.circle(30,360)
+
+p.pensize(7)
+p.bgcolor('light blue')
+draw_circle(0,0,'blue')
+draw_circle(60,0,'black')
+draw_circle(120, 0, 'red')
+draw_circle(90, -30, 'green')
+draw_circle(30, -30, 'yellow')
+p.hideturtle()
+p.done()
