4515a8509048dc5c07804820ba0e4113d946e85e
ltisoy <danieltisoy2001@gmail.com>
2022-04-27 13:49:49 -0500

    linter

--- simulando_iterador.py
+++ simulando_iterador.py
@@ -3,6 +3,7 @@ class Iterator:
     Iterador que devuelve los elementos de las posiciones
     pares de una lista
     """
+
     def __init__(self, data):
         self.data = data
         self.indice = 0
@@ -13,17 +14,19 @@ class Iterator:
     def __next__(self):
         if self.indice >= len(self.data):
             raise StopIteration
-        elem  = self.data[self.indice]
+            
+        elem = self.data[self.indice]
         self.indice += 2
         return elem
+
 
 "definimos objetos iteradores y hacemos pruebas"
 it = Iterator([])
 for e in it:
     print(e)
-it = Iterator([1,2,3,4,5,6])
+it = Iterator([1, 2, 3, 4, 5, 6])
 for e in it:
     print(e)
-it = Iterator([1,3,5,7,9])
+it = Iterator([1, 3, 5, 7, 9])
 for e in it:
     print(e)
