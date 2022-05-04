d73460e5deb2f7c765f580adda241c819edf77d3
ltisoy <danieltisoy2001@gmail.com>
2022-05-04 11:43:49 -0500

    se agregan condicionales según nuevo requerimiento

--- volumen_costo_paquete_reto_1.py
+++ volumen_costo_paquete_reto_1.py
@@ -8,6 +8,12 @@ profundo = float(input())
 volumen = alto * ancho * profundo
 # calcular costo de envio
 costo_envio = volumen * 5
+if alto>30:
+    costo_envio += 2000
+
+if costo_envio > 10000:
+    valor_iva = (costo_envio * 19)/100
+    costo_envio += valor_iva
 # imprimir volumen y costo
 print(volumen)
 print(costo_envio)
