28b5e716ae46373592a5860e9912059758c287d7
ltisoy <danieltisoy2001@gmail.com>
2022-05-07 13:33:24 -0500

    reto 3: agregar ciclo for
    ciclo for para leer varios paquetes a la vez e imprimir un costo total

--- volumen_costo_paquete_reto_1.py
+++ volumen_costo_paquete_reto_1.py
@@ -1,19 +1,26 @@
 # calcular volumen y costo de envio de un paquete
 # costo de envio = volumen * 5
-# crear tres variables [alto-ancho-profundo]
-alto = float(input())
-ancho = float(input())
-profundo = float(input())
-# calcular volumen
-volumen = alto * ancho * profundo
-# calcular costo de envio
-costo_envio = volumen * 5
-if alto>30:
-    costo_envio += 2000
+# agregar la caracteristica de calcular varios paquetes
+numeroPaquetes = int(input())
+costoTotal = 0
 
-if costo_envio > 10000:
-    valor_iva = (costo_envio * 19)/100
-    costo_envio += valor_iva
-# imprimir volumen y costo
-print(volumen)
-print(costo_envio)
+for i in range(1, numeroPaquetes+1):
+    # crear tres variables [alto-ancho-profundo]
+    alto = float(input())
+    ancho = float(input())
+    profundo = float(input())
+    # calcular volumen
+    volumen = alto * ancho * profundo
+    # calcular costo de envio
+    costo_envio = volumen * 5
+    if alto>30:
+        costo_envio += 2000
+
+    if costo_envio > 10000:
+        valor_iva = (costo_envio * 19)/100
+        costo_envio += valor_iva
+    costoTotal += costo_envio
+    # imprimir volumen y costo
+    print(volumen)
+    print(costo_envio)
+print(costoTotal)
