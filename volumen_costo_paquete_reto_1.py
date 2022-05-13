5cb772a78564475f8eac0b83a408014c46e429f8
ltisoy <danieltisoy2001@gmail.com>
2022-05-13 18:57:13 -0500

    reto 4 añadidas funciones y descuentos del valor total

--- volumen_costo_paquete_reto_1.py
+++ volumen_costo_paquete_reto_1.py
@@ -1,26 +1,37 @@
 # calcular volumen y costo de envio de un paquete
 # costo de envio = volumen * 5
-# agregar la caracteristica de calcular varios paquetes
-numeroPaquetes = int(input())
-costoTotal = 0
+def calcularCosto(alto, ancho, profundo):
 
-for i in range(1, numeroPaquetes+1):
-    # crear tres variables [alto-ancho-profundo]
-    alto = float(input())
-    ancho = float(input())
-    profundo = float(input())
-    # calcular volumen
     volumen = alto * ancho * profundo
-    # calcular costo de envio
-    costo_envio = volumen * 5
-    if alto>30:
-        costo_envio += 2000
+    costoEnvio = volumen * 5
 
-    if costo_envio > 10000:
-        valor_iva = (costo_envio * 19)/100
-        costo_envio += valor_iva
-    costoTotal += costo_envio
-    # imprimir volumen y costo
-    print(volumen)
-    print(costo_envio)
-print(costoTotal)
+    if alto>30:
+        costoEnvio += 2000
+
+    if costoEnvio > 10000:
+        valorIva = (costoEnvio * 19)/100
+        costoEnvio += valorIva
+
+    return costoEnvio
+
+def costoTotal(numeroPaquetes, descuento):
+
+    costoTotalEnvio = 0
+
+    for i in range(1, numeroPaquetes+1):
+        # crear tres variables [alto-ancho-profundo]
+        alto = float(input("inserte valor:"))
+        ancho = float(input("inserte valor:"))
+        profundo = float(input("inserte valor:"))
+
+        costoEnvio = calcularCosto(alto, ancho, profundo)
+
+        costoTotalEnvio += costoEnvio
+
+    costoDescuento = (costoTotalEnvio*descuento)/100
+    costoTotalEnvio -= costoDescuento
+
+    return costoTotalEnvio
+
+print(calcularCosto(35,10,5))
+print(costoTotal(2, 50))
